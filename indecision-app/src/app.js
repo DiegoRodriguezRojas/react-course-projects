91085a91a8c2947b71c6f77d30112127f15e1ef8
Diego Angel Rodriguez Rojas <kalel.12@hotmail.com>
2023-05-16 22:34:22 -0600

    Cap.24 Min.2

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,44 +1,57 @@
-console.log('app.js is running!');
+console.log('App.js is running!');
 
-//JSX - JavaScript XML
-
-var app = {
-    title: "Indecision App",
-    subtitle: "Put your life in the hands of a cumputer",
-    options: ['One', 'Two']
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
 };
 
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.title && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are you options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>
-    } else {
-        return 'unknown'
-    }
-}
+  const option = e.target.elements.option.value;
 
-var user = {
-    name: "Diego Angel Rodriguez Rojas",
-    age: 27,
-    location: "Mexico"
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
 };
-var templateTwo = (
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
     <div>
-        <h1>{user.name ? user.name : 'anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
-);
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
