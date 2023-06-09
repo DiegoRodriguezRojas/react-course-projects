16049fcab271ad8d0522568f4209f23275fe896a
Diego Angel Rodriguez Rojas <kalel.12@hotmail.com>
2023-06-08 21:19:45 -0600

    Cap.47

--- playground/counter-example.js
+++ playground/counter-example.js
@@ -1,11 +1,24 @@
 class Counter extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.handleAddOne = this.handleAddOne.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
       count: 0
+    };
+  }
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+
+    if (!isNaN(count)) {
+      this.setState(() => ({ count }));
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
     }
   }
   handleAddOne() {
@@ -29,7 +42,6 @@ class Counter extends React.Component {
       };
     });
   }
-
   render() {
     return (
       <div>
@@ -38,40 +50,8 @@ class Counter extends React.Component {
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
       </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-
-// let count = 0;
-// const addOne = () => {
-//   count++;
-//   renderCounterApp();
-// };
-// const minusOne = () => {
-//   count--;
-//   renderCounterApp();
-// };
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-// };
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
